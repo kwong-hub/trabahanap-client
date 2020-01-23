514149a98c2b905c5240c739d041e562e1fc8b0b
bereket-abera <bereket5042@gmail.com>
2020-01-23 15:32:53 +0300

    html prettier

--- src/app/applicant/components/add-applicant-profile/add-applicant-profile.component.ts
+++ src/app/applicant/components/add-applicant-profile/add-applicant-profile.component.ts
@@ -1,26 +1,35 @@
-import { ApplicantService } from '../../../_services/applicant.service';
-import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { LocationService } from '@app/_services/location.service';
-import _ from 'lodash';
-import { faCheck, faUserPlus, faIdCard, faCloudUploadAlt, faUserCheck, faEyeDropper, faEdit, faCamera, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { Router } from '@angular/router';
-import { AuthenticationService } from '@app/_services/authentication-service.service';
-import { ImageCroppedEvent } from 'ngx-image-cropper';
+import { ApplicantService } from "../../../_services/applicant.service";
+import { Component, OnInit, Input } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { LocationService } from "@app/_services/location.service";
+import _ from "lodash";
+import {
+  faCheck,
+  faUserPlus,
+  faIdCard,
+  faCloudUploadAlt,
+  faUserCheck,
+  faEyeDropper,
+  faEdit,
+  faCamera,
+  faTimes
+} from "@fortawesome/free-solid-svg-icons";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "@app/_services/authentication-service.service";
+import { ImageCroppedEvent } from "ngx-image-cropper";
 
 @Component({
-  selector: 'app-add-applicant-profile',
-  templateUrl: './add-applicant-profile.component.html',
-  styleUrls: ['./add-applicant-profile.component.scss']
+  selector: "app-add-applicant-profile",
+  templateUrl: "./add-applicant-profile.component.html",
+  styleUrls: ["./add-applicant-profile.component.scss"]
 })
 export class AddApplicantProfileComponent implements OnInit {
-
   faCheck = faCheck;
   faUserPlus = faUserPlus;
   faIdCard = faIdCard;
   faCloudUploadAlt = faCloudUploadAlt;
   faUserCheck = faUserCheck;
-  faEyeDropper= faEyeDropper;
+  faEyeDropper = faEyeDropper;
   faEdit = faEdit;
   faCamera = faCamera;
   faTimes = faTimes;
@@ -32,70 +41,118 @@ export class AddApplicantProfileComponent implements OnInit {
   regions: any;
   countries: any;
   cities: any;
-  genderOptions: any = [{name: 'MALE', value: 'MALE'}, {name: 'FEMALE', value: 'FEMALE'}];
-  monthOptions = [{name: 'January' , value: '01'}, {name: 'February' , value: '02'},
-    {name: 'March' , value: '03'}, {name: 'April' , value: '04'}, {name: 'May' , value: '05'}, 
-    {name: 'June' , value: '06'}, {name: 'July' , value: '07'}, {name: 'August' , value: '08'}, 
-    {name: 'September' , value: '09'}, {name: 'October' , value: '10'},
-    {name: 'November' , value: '11'}, {name: 'December' , value: '12'}]
-  hasProfile:boolean = false;
-  submitted:boolean = false;
-  styleObject = {'inputContainer': {},'input':{fontSize:'1.7rem'},'inputHeader': {fontSize: "1.7rem", borderBottom: "1px solid #888"}, 'optionContainer': {backgroundColor: "#555", top: "3.3rem", boxShadow: '0px 1px 2px #aaa'}, 'option': {fontSize: "1.5rem", borderBottom: "1px solid #ddd", backgroundColor: '#fff'}};
-  monthStyle = {'inputContainer': {margin: "0"}, 'inputHeader': {fontSize: "1.5rem", borderBottom: "1px solid #888"}, 'optionContainer': {backgroundColor: "#555", top: "3.3rem", boxShadow: '0px 1px 2px #aaa'}, 'option': {fontSize: "1.5rem", borderBottom: "1px solid #ddd", backgroundColor: '#fff'}};
-  dateStyle = {'inputContainer': {marginRight: "0"}, 'input': {}, 'label': {}, 'feedbackContainer': {}, 'feedbackMessage': {}};
+  genderOptions: any = [
+    { name: "MALE", value: "MALE" },
+    { name: "FEMALE", value: "FEMALE" }
+  ];
+  monthOptions = [
+    { name: "January", value: "01" },
+    { name: "February", value: "02" },
+    { name: "March", value: "03" },
+    { name: "April", value: "04" },
+    { name: "May", value: "05" },
+    { name: "June", value: "06" },
+    { name: "July", value: "07" },
+    { name: "August", value: "08" },
+    { name: "September", value: "09" },
+    { name: "October", value: "10" },
+    { name: "November", value: "11" },
+    { name: "December", value: "12" }
+  ];
+  hasProfile: boolean = false;
+  submitted: boolean = false;
+  styleObject = {
+    inputContainer: {},
+    input: { fontSize: "1.7rem" },
+    inputHeader: { fontSize: "1.7rem", borderBottom: "1px solid #888" },
+    optionContainer: {
+      backgroundColor: "#555",
+      top: "3.3rem",
+      boxShadow: "0px 1px 2px #aaa"
+    },
+    option: {
+      fontSize: "1.5rem",
+      borderBottom: "1px solid #ddd",
+      backgroundColor: "#fff"
+    }
+  };
+  monthStyle = {
+    inputContainer: { margin: "0" },
+    inputHeader: { fontSize: "1.5rem", borderBottom: "1px solid #888" },
+    optionContainer: {
+      backgroundColor: "#555",
+      top: "3.3rem",
+      boxShadow: "0px 1px 2px #aaa"
+    },
+    option: {
+      fontSize: "1.5rem",
+      borderBottom: "1px solid #ddd",
+      backgroundColor: "#fff"
+    }
+  };
+  dateStyle = {
+    inputContainer: { marginRight: "0" },
+    input: {},
+    label: {},
+    feedbackContainer: {},
+    feedbackMessage: {}
+  };
   formErrors = ["Some form elements are not valid."];
   cvFileTypes = ".pdf,.doc,.docx";
   profilePictureFileTypes = ".png, .jpg, jpeg";
-  inputType: string = 'file';
+  inputType: string = "file";
   showLoader = false;
   showCVPreview = false;
 
   isCVEditModalOpen = false;
   isApplicantPictureEditModalOpen = false;
-  yearRange = {min: "1920", max: "2020"}
-  dateRange = {min: "1", max: "31"}
+  yearRange = { min: "1920", max: "2020" };
+  dateRange = { min: "1", max: "31" };
   imageChangedEvent: any;
   croppedImage: any;
   tempImg: string;
-  
+
   constructor(
     private formBuilder: FormBuilder,
     private locationService: LocationService,
     private applicantService: ApplicantService,
-    private router: Router, 
+    private router: Router,
     private authService: AuthenticationService
-  ) { }
+  ) {}
 
   ngOnInit() {
-
     this.getRegions();
     this.getCountries();
 
     this.addApplicantProfileForm = this.formBuilder.group({
-      currentEmployer: [''],
-      currentOccopation: [''],
-      address: [''],
-      gender: ['', Validators.required],
-      year: ['', Validators.required],
-      month: ['', Validators.required],
-      date: ['', Validators.required],
-      dateOfBirth: ['', Validators.required],
-      selfDescription: ['', [Validators.required, Validators.maxLength(1500)]],
-      cv: ['', Validators.required],
-      applicantPicture: [''],
-      CityId: ['', Validators.required],
-      RegionId: ['', Validators.required],
-      CountryId: ['', Validators.required]
-    })
+      currentEmployer: [""],
+      currentOccopation: [""],
+      address: [""],
+      gender: ["", Validators.required],
+      year: ["", Validators.required],
+      month: ["", Validators.required],
+      date: ["", Validators.required],
+      dateOfBirth: ["", Validators.required],
+      selfDescription: ["", [Validators.required, Validators.maxLength(1500)]],
+      cv: ["", Validators.required],
+      applicantPicture: [""],
+      CityId: ["", Validators.required],
+      RegionId: ["", Validators.required],
+      CountryId: ["", Validators.required]
+    });
 
-    if(this.applicantProfile){
+    if (this.applicantProfile) {
       let temp_date = new Date(this.applicantProfile.dateOfBirth);
 
-      this.applicantProfile = {...this.applicantProfile, year: temp_date.getFullYear(),
-        month: temp_date.getMonth() + 1, date: temp_date.getDate() }
+      this.applicantProfile = {
+        ...this.applicantProfile,
+        year: temp_date.getFullYear(),
+        month: temp_date.getMonth() + 1,
+        date: temp_date.getDate()
+      };
 
       this.hasProfile = true;
-      this.inputType = 'text';
+      this.inputType = "text";
       this.updateForm();
       this.disableEdit();
       this.getCitiesByRegionId(this.applicantProfile.RegionId);
@@ -103,18 +160,22 @@ export class AddApplicantProfileComponent implements OnInit {
     }
   }
 
-  selectChanged(value, name){
-    if(name == "RegionId"){
+  selectChanged(value, name) {
+    if (name == "RegionId") {
       this.getCitiesByRegionId(value);
       this.addApplicantProfileForm.controls[name].setValue(value);
-      this.applicantProfile = {...this.applicantProfile, CityId: ''}
+      this.applicantProfile = { ...this.applicantProfile, CityId: "" };
     }
     this.addApplicantProfileForm.controls[name].setValue(value);
   }
 
-  updateForm(){
+  updateForm() {
     _.map(this.applicantProfile, (value, key) => {
-      if(this.addApplicantProfileForm.controls[key] && key != 'cv' && key !='applicantPicture'){
+      if (
+        this.addApplicantProfileForm.controls[key] &&
+        key != "cv" &&
+        key != "applicantPicture"
+      ) {
         this.addApplicantProfileForm.controls[key].setValue(value);
       }
     });
@@ -135,105 +196,118 @@ export class AddApplicantProfileComponent implements OnInit {
   }
 
   closeImageModal() {
-    this.imageChangedEvent = '';
-    this.selectedImage = ''
+    this.imageChangedEvent = "";
+    this.selectedImage = "";
   }
 
   saveImage() {
     this.tempImg = this.croppedImage.base64;
     this.closeImageModal();
-    const byteCharacters = atob(this.tempImg.split(',')[1]);
+    const byteCharacters = atob(this.tempImg.split(",")[1]);
     const byteNumbers = new Array(byteCharacters.length);
     for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
     }
     const byteArray = new Uint8Array(byteNumbers);
-    const blob = new Blob([byteArray], {type: 'image/png'});
+    const blob = new Blob([byteArray], { type: "image/png" });
 
-    console.log(blob)
-    this.formData.append('applicantPicture', blob);
+    console.log(blob);
+    this.formData.append("applicantPicture", blob);
   }
 
-  onSubmit(){
+  onSubmit() {
     this.submitted = true;
     let val = this.addApplicantProfileForm.value;
     let date = `${val.year}-${val.month}-${val.date}`;
-    
-    if(new Date(date).toDateString().includes("Invalid")) {
-      this.addApplicantProfileForm.controls['month'].setErrors({'invalid': true});
+
+    if (new Date(date).toDateString().includes("Invalid")) {
+      this.addApplicantProfileForm.controls["month"].setErrors({
+        invalid: true
+      });
       return;
     }
 
-    this.addApplicantProfileForm.controls['dateOfBirth'].setValue(date);
+    this.addApplicantProfileForm.controls["dateOfBirth"].setValue(date);
     // console.log(this.addApplicantProfileForm.value);
-    if(this.addApplicantProfileForm.invalid){ return; }
-    
+    if (this.addApplicantProfileForm.invalid) {
+      return;
+    }
+
     // new form value after date of birth is added
     val = this.addApplicantProfileForm.value;
     this.showLoader = true;
     var vals = this.addApplicantProfileForm.value;
     _.map(vals, (value, key) => {
-      console.log(key,"=>", value)
-      if(key != 'cv' && key !='applicantPicture'){
+      console.log(key, "=>", value);
+      if (key != "cv" && key != "applicantPicture") {
         this.formData.append(key, value);
       }
-    })
+    });
     // console.log(this.addApplicantProfileForm.value);
 
-    this.applicantService.addApplicantProfileWithCV(this.formData)
-      .subscribe(
-        data => {
-          console.log(data)
-          if(data.success){
-            this.showLoader = false;
-            let currentUser = this.authService.currentUserValue;
-            this.authService.updateCurrentUser({...currentUser, hasFinishedProfile:true, applicantProfile: data.applicantProfile});
-            this.router.navigate(['/applicant/jobs']);
-          }
-          else {
-            this.submitted = false;
-            this.showLoader = false;
-          }
-        },
-        err => {
-          console.log(err);
+    this.applicantService.addApplicantProfileWithCV(this.formData).subscribe(
+      data => {
+        console.log(data);
+        if (data.success) {
+          this.showLoader = false;
+          let currentUser = this.authService.currentUserValue;
+          this.authService.updateCurrentUser({
+            ...currentUser,
+            hasFinishedProfile: true,
+            applicantProfile: data.applicantProfile
+          });
+          this.router.navigate(["/applicant/jobs"]);
+        } else {
+          this.submitted = false;
           this.showLoader = false;
         }
-      );
-
+      },
+      err => {
+        console.log(err);
+        this.showLoader = false;
+      }
+    );
   }
 
-  onEdit(){
+  onEdit() {
     this.submitted = true;
     var val = this.addApplicantProfileForm.value;
     let date = `${val.year}-${val.month}-${val.date}`;
-    
-    if(new Date(date).toDateString().includes("Invalid")) {
-      this.addApplicantProfileForm.controls['month'].setErrors({'invalid': true});
+
+    if (new Date(date).toDateString().includes("Invalid")) {
+      this.addApplicantProfileForm.controls["month"].setErrors({
+        invalid: true
+      });
       return;
     }
 
-    this.addApplicantProfileForm.controls['dateOfBirth'].setValue(date);
-    if(this.addApplicantProfileForm.invalid){ return; }
+    this.addApplicantProfileForm.controls["dateOfBirth"].setValue(date);
+    if (this.addApplicantProfileForm.invalid) {
+      return;
+    }
 
     val = this.addApplicantProfileForm.value;
     this.showLoader = true;
 
     _.map(val, (value, key) => {
       // console.log(key,"=>", value)
-      if(key != 'cv' && key !='applicantPicture'){
+      if (key != "cv" && key != "applicantPicture") {
         this.formData.append(key, value);
       }
-    })
+    });
 
-    this.applicantService.editApplicantProfile(this.formData, this.applicantProfile.id)
+    this.applicantService
+      .editApplicantProfile(this.formData, this.applicantProfile.id)
       .subscribe(
         data => {
-          console.log(data)
-          if(data.success){
+          console.log(data);
+          if (data.success) {
             this.showLoader = false;
             let currentUser = this.authService.currentUserValue;
-            this.authService.updateCurrentUser({...currentUser, applicantProfile: data.applicantProfile});
+            this.authService.updateCurrentUser({
+              ...currentUser,
+              applicantProfile: data.applicantProfile
+            });
             this.disableEdit();
           }
         },
@@ -244,68 +318,67 @@ export class AddApplicantProfileComponent implements OnInit {
       );
   }
 
-  getCitiesByRegionId(regionId){
-    this.locationService.getAllRegionCities(regionId)
-      .subscribe(
-        response => {
-          const cities = response.cities;
-          this.cities = [];
-          cities.map(city => {
-            this.cities.push({name: city.cityName, value: city.id})
-          })
-        },
-        error => console.log(error)
-      )
+  getCitiesByRegionId(regionId) {
+    this.locationService.getAllRegionCities(regionId).subscribe(
+      response => {
+        const cities = response.cities;
+        this.cities = [];
+        cities.map(city => {
+          this.cities.push({ name: city.cityName, value: city.id });
+        });
+      },
+      error => console.log(error)
+    );
   }
 
-  getRegions(){
-    this.locationService.getAllRegions()
-      .subscribe(
-        response => {
-          const regions = response.regions;
-          this.regions = [];
-          regions.map(region => {
-            this.regions.push({name: region.regionName, value: region.id})
-          })
-        },
-        error => console.log(error)
-      )
+  getRegions() {
+    this.locationService.getAllRegions().subscribe(
+      response => {
+        const regions = response.regions;
+        this.regions = [];
+        regions.map(region => {
+          this.regions.push({ name: region.regionName, value: region.id });
+        });
+      },
+      error => console.log(error)
+    );
   }
 
-  getCountries(){
-    this.locationService.getAllCountries()
-      .subscribe(
-        response => {
-          const countries = response.countries;
-          this.countries = [];
-          countries.map(country => {
-            this.countries.push({name: country.countryName, value: country.id})
-          })
+  getCountries() {
+    this.locationService.getAllCountries().subscribe(
+      response => {
+        const countries = response.countries;
+        this.countries = [];
+        countries.map(country => {
+          this.countries.push({ name: country.countryName, value: country.id });
+        });
 
-          this.addApplicantProfileForm.controls['CountryId'].setValue(countries[0].id);
-        },
-        error => console.log(error)
-      )
+        this.addApplicantProfileForm.controls["CountryId"].setValue(
+          countries[0].id
+        );
+      },
+      error => console.log(error)
+    );
   }
 
-  enableEdit(event){
+  enableEdit(event) {
     event.stopPropagation();
     this.hasProfile = false;
     _.map(this.applicantProfile, (value, key) => {
-      if(this.addApplicantProfileForm.controls[key]){
-        if(!['cv'].includes(key)){
+      if (this.addApplicantProfileForm.controls[key]) {
+        if (!["cv"].includes(key)) {
           this.addApplicantProfileForm.controls[key].enable();
-        } 
+        }
       }
     });
     this.addApplicantProfileForm.controls["year"].enable();
     this.addApplicantProfileForm.controls["date"].enable();
   }
 
-  disableEdit(){
+  disableEdit() {
     this.hasProfile = true;
     _.map(this.applicantProfile, (value, key) => {
-      if(this.addApplicantProfileForm.controls[key]){
+      if (this.addApplicantProfileForm.controls[key]) {
         this.addApplicantProfileForm.controls[key].disable();
       }
     });
@@ -313,28 +386,35 @@ export class AddApplicantProfileComponent implements OnInit {
     this.addApplicantProfileForm.controls["date"].disable();
   }
 
-  onCVPreview(event){
+  onCVPreview(event) {
     event.stopPropagation();
     this.showCVPreview = !this.showCVPreview;
   }
 
-  toggleCVModal(event){
+  toggleCVModal(event) {
     this.isCVEditModalOpen = !this.isCVEditModalOpen;
   }
 
-  toggleApplicantPictureModal(){
-    this.isApplicantPictureEditModalOpen = !this.isApplicantPictureEditModalOpen;
+  toggleApplicantPictureModal() {
+    this.isApplicantPictureEditModalOpen = !this
+      .isApplicantPictureEditModalOpen;
   }
 
-  editCVChanged(event){
+  editCVChanged(event) {
     console.log(event);
   }
 
-  applicantUpdated(event){
+  applicantUpdated(event) {
     this.applicantProfile = event;
-    this.applicantProfile.dateOfBirth = this.applicantProfile.dateOfBirth ? this.applicantProfile.dateOfBirth.split('T')[0] : null;
+    this.applicantProfile.dateOfBirth = this.applicantProfile.dateOfBirth
+      ? this.applicantProfile.dateOfBirth.split("T")[0]
+      : null;
     let currentUser = this.authService.currentUserValue;
-    this.authService.updateCurrentUser({...currentUser, hasFinishedProfile:true, applicantProfile: event});        
+    this.authService.updateCurrentUser({
+      ...currentUser,
+      hasFinishedProfile: true,
+      applicantProfile: event
+    });
     this.updateForm();
     // console.log(event);
   }
