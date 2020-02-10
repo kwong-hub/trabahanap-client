f9b069b014bbf5e0a97014f6a5f4035c1960d658
Silas <silogecho@gmail.com>
2020-02-10 15:50:36 +0300

    temporary email verification send email

--- src/app/_services/admin.service.ts
+++ src/app/_services/admin.service.ts
@@ -242,4 +242,10 @@ export class AdminService {
   getAllReportedJob() {
     return this.http.get<any>(`${environment.apiUrl}/admin/reports`);
   }
+  verifyUser(){
+    return this.http.post<any>(`${environment.apiUrl}/admin/send_email`,{});
+  }
+  unVerifiedUser(){
+    return this.http.get<any>(`${environment.apiUrl}/admin/find_email`)
+  }
 }
