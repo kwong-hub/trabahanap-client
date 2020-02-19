ae8dd98399d46b41c2e16859520cc831e29d74ad
kaleb <kaleb.wond@gmail.com>
2020-02-19 15:29:22 +0300

    changes yet again

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.0.105:3000/api'
   // apiUrl: 'http://192.168.0.105:3000/api'
+  // apiUrl: 'http://localhost:3000/api'
   // apiUrl: 'http://192.168.0.105:3000/api'
-  // apiUrl: 'http://192.168.0.105:3000/api'
+  apiUrl: 'http://192.168.0.103:3000/api'
 };
